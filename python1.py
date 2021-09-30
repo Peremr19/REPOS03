032f04f2d0c58514795a767a44e15070bf6cd246
litisqui <angulogomezsergio@gmail.com>
2021-09-30 16:29:49 +0200

    Modifico sergio

--- python1.py
+++ python1.py
@@ -5,6 +5,7 @@
 # Se usa la función bin() que transforma e.g. 3 en "0b11".
 # En esta función se quita el "0b" para dejar el "11"
 # ------------------------------------------------------
+#modificación realizada
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
     numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
